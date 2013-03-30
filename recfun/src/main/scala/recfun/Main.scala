8a7f4b01bcb56ac78d43f6f4eaeb2c6cde1c523c
Sergey Parshukov <sergey@robotlovesyou.ru>
2013-03-31 03:08:34 +0400

    Coin change problem

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -41,7 +41,19 @@ object Main {
   }
 
   /**
-   * Change variants count
+   * Coin change problem
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def count(money: Int, coins: List[Int]): Int = {
+      if (money == 0)
+        1
+      else if (money < 0)
+        0
+      else if (coins.isEmpty && money >= 1)
+        0
+      else
+        count(money, coins.tail) + count(money - coins.head, coins)
+    }
+    count(money, coins)
+  }
 }
