eee91f4fc36f9a028cea0a36a195628a53fad972
Sergey Parshukov <sergey@robotlovesyou.ru>
2013-05-19 21:23:36 +0400

    Bloxorz solution

--- streams/src/main/scala/streams/Solver.scala
+++ streams/src/main/scala/streams/Solver.scala
@@ -10,7 +10,10 @@ trait Solver extends GameDef {
   /**
    * Returns `true` if the block `b` is at the final position
    */
-  def done(b: Block): Boolean = ???
+  def done(b: Block): Boolean = b.b1 == goal && b.b2 == goal
+
+  type Moves = List[Move]
+  type StateHistory = Stream[(Block, Moves)]
 
   /**
    * This function takes two arguments: the current block `b` and
@@ -28,15 +31,20 @@ trait Solver extends GameDef {
    * It should only return valid neighbors, i.e. block positions
    * that are inside the terrain.
    */
-  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = ???
+  def neighborsWithHistory(b: Block, history: Moves): StateHistory =
+    b.legalNeighbors.map{
+        case (block, move) => (block, move :: history)
+    }.toStream
 
   /**
    * This function returns the list of neighbors without the block
    * positions that have already been explored. We will use it to
    * make sure that we don't explore circular paths.
    */
-  def newNeighborsOnly(neighbors: Stream[(Block, List[Move])],
-                       explored: Set[Block]): Stream[(Block, List[Move])] = ???
+  def newNeighborsOnly(neighbors: StateHistory, explored: Set[Block]): StateHistory =
+    neighbors.filter{
+      case (block, move) => !explored.contains(block)
+    }
 
   /**
    * The function `from` returns the stream of all possible paths
@@ -61,19 +69,34 @@ trait Solver extends GameDef {
    * of different paths - the implementation should naturally
    * construct the correctly sorted stream.
    */
-  def from(initial: Stream[(Block, List[Move])],
-           explored: Set[Block]): Stream[(Block, List[Move])] = ???
+  def from(initial: StateHistory, explored: Set[Block]): StateHistory = {
+    if (initial.isEmpty) {
+      Stream.empty
+    } else {
+      val newMoves = initial.flatMap {
+        case (block, moves) => newNeighborsOnly(
+          neighborsWithHistory(block, moves),
+          explored
+        )
+      }
+      initial ++ from(newMoves, explored ++ newMoves.map(_._1))
+    }
+  }
 
   /**
    * The stream of all paths that begin at the starting block.
    */
-  lazy val pathsFromStart: Stream[(Block, List[Move])] = ???
+  lazy val pathsFromStart: StateHistory = 
+    from(Stream((startBlock, Nil)), Set())
 
   /**
    * Returns a stream of all possible pairs of the goal block along
    * with the history how it was reached.
    */
-  lazy val pathsToGoal: Stream[(Block, List[Move])] = ???
+  lazy val pathsToGoal: StateHistory =
+    pathsFromStart.filter {
+      case (block, moves) => done(block)
+    }
 
   /**
    * The (or one of the) shortest sequence(s) of moves to reach the
@@ -83,5 +106,10 @@ trait Solver extends GameDef {
    * the first move that the player should perform from the starting
    * position.
    */
-  lazy val solution: List[Move] = ???
+  lazy val solution: Moves = pathsToGoal match {
+    case x #:: xs => x match {
+      case (block, moves) => moves
+    }
+    case _ => Nil
+  }
 }
--- streams/src/test/scala/streams/BloxorzSuite.scala
+++ streams/src/test/scala/streams/BloxorzSuite.scala
@@ -53,15 +53,53 @@ class BloxorzSuite extends FunSuite {
     }
   }
 
+  test("isLegal") {
+    new Level1 {
+      assert(Block(Pos(0, 0), Pos(0, 1)).isLegal)
+      assert(!Block(Pos(2, 0), Pos(3, 0)).isLegal)
+    }
+  }
+
+  test("neighborsWithHistory") {
+    new Level1 {
+      assert(
+        neighborsWithHistory(
+          Block(Pos(1,1), Pos(1,1)),
+          List(Left,Up)
+        ).toSet === Set(
+          (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+          (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+        )
+      )
+    }
+  }
+
+  test("newNeighborsOnly") {
+    new Level1 {
+      assert(
+        newNeighborsOnly(
+          Set(
+            (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+            (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+          ).toStream,
+
+          Set(Block(Pos(1,2),Pos(1,3)), Block(Pos(1,1),Pos(1,1)))
+        ) === Set(
+          (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+        ).toStream
+      )
+    }
+  }
+
   test("optimal solution for level 1") {
     new Level1 {
-      assert(solve(solution) == Block(goal, goal))
+      assert(solve(solution) === Block(goal, goal))
     }
   }
 
   test("optimal solution length for level 1") {
     new Level1 {
-      assert(solution.length == optsolution.length)
+      assert(solution.length === optsolution.length)
     }
   }
 }
